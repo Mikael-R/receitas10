5416acc93f9b94545060370baa02ffadd4aee216
Mikael-R <mikarg9@gmail.com>
2021-03-07 13:12:57 -0300

    feat: create session controller to log-in user

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,31 +1,39 @@
-import 'dotenv/config'
-
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
 import { Request, Response } from 'express'
 
-// import User from '../models/User'
+import userRepository from '../repository/user-repository'
 
 class SessionController {
-  async store (req: Request, res: Response) {
+  async store(req: Request, res: Response) {
     const { email, password } = req.body
+    if (!email || !password) {
+      return res
+        .status(422)
+        .json({ error: true, message: 'Parâmetros faltando' })
+    }
 
-    const user = {
-      id: 'id-teste',
-      email: 'mikael@mail.com',
-      passwordHash: ''
-    } // procura o user no banco pelo email
-
-    if (!user) return res.status(404).send('User not found')
+    const user = await userRepository.findByEmail(email)
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: true, message: 'Usuário não encontrado' })
+    }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash)
-
-    // if (!isPasswordCorrect) return res.status(401).send('Wrong password')
+    if (!isPasswordCorrect)
+      return res.status(401).json({ error: true, message: 'Senha incorreta' })
 
     return res.json({
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET, {
-        expiresIn: '7d'
-      })
+      error: false,
+      message: 'Usuário encontrado',
+      user: {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        token: user.token,
+        avatarUrl: user.avatarUrl,
+        description: user.description,
+      },
     })
   }
 }
