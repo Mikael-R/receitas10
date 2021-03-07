4c6a1289e44e45db17bcc24e337511a60e247b87
Mikael-R <mikarg9@gmail.com>
2021-03-07 13:11:24 -0300

    feat: create user controller to sign-up user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,54 @@
 import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import { v4 as uuidv4 } from 'uuid'
+
+import userRepository from '../repository/user-repository'
 
 class UserController {
-  async store (req: Request, res: Response) {
-    const { email, password } = req.body
+  async store(req: Request, res: Response) {
+    const { name, username, email, password } = req.body
+    if (!name || !username || !email || !password)
+      return res
+        .status(422)
+        .json({ error: true, message: 'Parâmetros faltando' })
+    if (username.length < 2 || username.length > 12)
+      return res.status(403).json({
+        error: true,
+        message: 'Username não pode ser menor que 2 ou maior que 12 caracteres',
+      })
 
-    // verifica se o email ja está cadastrado
-    // cadastra user no banco
-    // salva apenas hash da senha
-    // retorna id do usuário no banco
+    const existsUsername = await userRepository.existsUsername(username)
+    if (existsUsername)
+      return res
+        .status(409)
+        .json({ error: true, message: 'Este username já existe' })
+    const existsEmail = await userRepository.existsEmail(email)
+    if (existsEmail)
+      return res
+        .status(409)
+        .json({ error: true, message: 'Este email já existe' })
 
-    const userId = 'id-teste'
+    const userId = uuidv4()
+    const passwordHash = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ userId }, process.env.APP_SECRET, {
+      expiresIn: '7d',
+    })
 
-    return res.json({ userId })
+    await userRepository.createUser({
+      id: userId,
+      username,
+      name,
+      email,
+      passwordHash,
+      token,
+    })
+
+    return res.json({
+      error: false,
+      message: 'Usuário criado com sucesso',
+      userId,
+    })
   }
 }
 
