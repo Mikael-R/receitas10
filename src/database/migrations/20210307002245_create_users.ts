d880ae9a7b3495c311833e2e63f69047e8ea6d49
Mikael-R <mikarg9@gmail.com>
2021-03-09 13:01:35 -0300

    fix: table parameters

--- src/database/migrations/20210307002245_create_users.ts
+++ src/database/migrations/20210307002245_create_users.ts
@@ -4,16 +4,14 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', function (table) {
     table.uuid('id').primary().notNullable().unique()
     table.string('username', 12).notNullable().unique()
-    table.text('name').notNullable()
+    table.string('name').notNullable()
     table.string('email').notNullable().unique()
     table.string('passwordHash').notNullable()
     table.string('token').notNullable().unique()
     table.string('avatarUrl')
-    table.string('description')
-    table
-      .timestamp('createAccountAt')
-      .notNullable()
-      .defaultTo(new Date().getTime())
+    table.text('description')
+    table.timestamp('createdAt').notNullable().defaultTo(new Date().getTime())
+    table.timestamp('updatedAt').notNullable().defaultTo(new Date().getTime())
     table.timestamp('lastSessionAt')
   })
 }
