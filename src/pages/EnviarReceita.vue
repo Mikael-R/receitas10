56a8d82db8e6cc52ac66da5035ee1cf1cbcad532
renatoReboucas <renato190698@gmail.com>
2021-03-12 22:52:41 -0300

    wip

--- src/pages/EnviarReceita.vue
+++ src/pages/EnviarReceita.vue
@@ -2,7 +2,23 @@
 <div>
   <Nav :desabilitar="desabilitar"/>
   <div class="container">
-    <h1>/enviar1</h1>
+    <div class="row">
+      <div class="col">
+        <h1 class="title">Enviar uma receita</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <span class="input-name">Nome</span>
+        <input type="text" class="form-control " placeholder="Diga o nome da sua receita...">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <span class="input-name">Ingredientes</span>
+        <textarea type="text" class="form-control" rows="3" placeholder="Diga os ingredientes da sua receita...">
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -22,5 +38,23 @@ components:{
 </script>
 
 <style scoped>
-
+.title{
+  margin-top: 43px;
+  font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 35px;
+line-height: 52px;
+letter-spacing: 0.035em;
+color: #000000;
+}
+.input-name{
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 22px;
+line-height: 33px;
+letter-spacing: 0.035em;
+color: #000000;
+}
 </style>
