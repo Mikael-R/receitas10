253cc1f4886b837e712f67657dc0d663f9101e1c
Mikael-R <mikarg9@gmail.com>
2021-03-14 16:19:39 -0300

    cria funcao para pegar receitas de usuario

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -4,4 +4,7 @@ export default {
   listRandom: (page = 1) => {
     return http.get(`recipes/random?page=${page}`);
   },
+  userRecipes: (username) => {
+    return http.get(`/recipes/${username}`)
+  }
 };
