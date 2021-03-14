83f629b7c384651287977204d66d1991e811b521
Mikael-R <mikarg9@gmail.com>
2021-03-14 12:49:31 -0300

    chore: enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
 
 import './database/connection'
@@ -9,6 +10,7 @@ const app = express()
 const port = process.env.PORT || 3333
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 app.use(routes)
 
